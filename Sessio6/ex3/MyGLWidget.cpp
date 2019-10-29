187a031b9b2d14afea0c154dda5258c17a7a3b2d
nilfm <nil.fons@gmail.com>
2019-10-29 17:26:45 +0100

    Sessio6

---
+++ Sessio6/ex3/MyGLWidget.cpp
@@ -0,0 +1,341 @@
+#include "MyGLWidget.h"
+
+#include <iostream>
+
+MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent), program(NULL)
+{
+  setFocusPolicy(Qt::StrongFocus);  // per rebre events de teclat
+  scale = 1.0f;
+  angle = 0.0f;
+  psi = theta = phi = 0.0f;
+  perspective = true;
+}
+
+MyGLWidget::~MyGLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();  
+
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  carregaShaders();
+  creaBuffers();
+  glEnable(GL_DEPTH_TEST);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  min_scene = glm::vec3(-2.5, 0, -2.5);
+  max_scene = glm::vec3(2.5, 4.0, 2.5);
+  calcCenterRadius();
+  calcModelBox();
+  iniCamera();
+}
+
+void MyGLWidget::iniCamera() {
+	dist = radius*2.0;
+	
+	OBS = glm::vec3(0, 2.0, dist);
+	VRP = glm::vec3(center);
+	VUP = glm::vec3(0, 1, 0);
+	viewTransform();
+	
+	znear = dist-radius;
+	zfar = dist+radius;
+	fov = 2.0*asin(radius/dist);
+	fov_ini = fov;
+	ra = 1.0f;
+	projectTransform();
+}
+
+void MyGLWidget::paintGL () 
+{
+// En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
+// useu els paràmetres que considereu (els que hi ha són els de per defecte)
+//  glViewport (0, 0, ample, alt);
+  
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Activem el VAO per a pintar la caseta 
+  glBindVertexArray(VAO_Model);
+
+  // Carreguem la transformació de model
+  modelTransform(false);
+  projectTransform();
+  viewTransform();
+
+  // pintem
+  glDrawArrays(GL_TRIANGLES, 0, m.faces().size()*3);
+
+
+  glBindVertexArray(VAO_Terra);
+  modelTransform(true);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::modelTransform(bool terra) 
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  if (not terra) {
+	  transform = glm::rotate(transform, angle, glm::vec3(0, 1, 0));
+	  transform = glm::scale(transform, glm::vec3(scale));
+	  transform = glm::scale(transform, glm::vec3(scale2));
+	  transform = glm::translate(transform, -patricio_center_base);
+  }
+  else {
+	transform = glm::scale(transform, glm::vec3(scale));
+  }
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::projectTransform() {
+	//Arguments: FOV (radians), aspect ratio window, znear, zfar
+	glm::mat4 Proj(1.0f);
+	if (perspective) Proj = glm::perspective(fov, ra, znear, zfar);
+	else {
+		Proj = glm::ortho(min_scene.x, max_scene.x, min_scene.z, max_scene.z, znear, zfar);
+	 }
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, &Proj[0][0]);
+}
+
+void MyGLWidget::viewTransform() {
+	//glm::mat4 View = glm::lookAt(OBS, VRP, VUP);
+	glm::mat4 View(1.0f);
+	View = glm::translate(View, glm::vec3(0.0, 0.0, -5.0));
+	View = glm::rotate(View, -phi, glm::vec3(0, 0, 1));
+	View = glm::rotate(View, theta, glm::vec3(1, 0, 0));
+	View = glm::rotate(View, -psi, glm::vec3(0, 1, 0));
+	View = glm::translate(View, -VRP);
+	
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &View[0][0]);
+}
+
+void MyGLWidget::calcCenterRadius() {
+	center = (min_scene+max_scene)/glm::vec3(2.0);
+	radius = distance(min_scene, max_scene)/2.0;
+}
+
+void MyGLWidget::calcModelBox() {
+  // Càlcul capsa contenidora i valors transformacions inicials
+  float minx, miny, minz, maxx, maxy, maxz;
+  minx = maxx = m.vertices()[0];
+  miny = maxy = m.vertices()[1];
+  minz = maxz = m.vertices()[2];
+  for (unsigned int i = 3; i < m.vertices().size(); i+=3)
+  {
+    if (m.vertices()[i+0] < minx)
+      minx = m.vertices()[i+0];
+    if (m.vertices()[i+0] > maxx)
+      maxx = m.vertices()[i+0];
+    if (m.vertices()[i+1] < miny)
+      miny = m.vertices()[i+1];
+    if (m.vertices()[i+1] > maxy)
+      maxy = m.vertices()[i+1];
+    if (m.vertices()[i+2] < minz)
+      minz = m.vertices()[i+2];
+    if (m.vertices()[i+2] > maxz)
+      maxz = m.vertices()[i+2];
+  }
+  
+  
+  std::cout << std::endl << 
+  "MIN(x,y,z) = (" << minx << "," << miny << "," << minz << ")" << std::endl <<
+  "MAX(x,y,z) = (" << maxx << "," << maxy << "," << maxz << ")" << std::endl;
+   scale2 = 4.0/(maxy-miny);
+   patricio_center_base = glm::vec3((minx+maxx)/2, miny, (minz+maxz)/2);
+ }
+
+void MyGLWidget::resizeGL (int w, int h) 
+{
+  float rav = float(w)/float(h);
+  ra = rav;
+  if (rav < 1.0) fov = 2.0*atan(tan(fov_ini/2.0)/rav);
+  projectTransform();
+  glViewport(0, 0, w, h);
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event) 
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_S: { // escalar a més gran
+      scale += 0.05;
+      break;
+    }
+    case Qt::Key_D: { // escalar a més petit
+      scale -= 0.05;
+      break;
+    }
+    case Qt::Key_R: {
+	  angle += M_PI/4.0f;
+	  break;
+	}
+    case Qt::Key_O: {
+      perspective = !perspective;
+	  break;
+	}
+    case Qt::Key_Z: {
+      fov -= 0.1;
+	  break;
+	}
+    case Qt::Key_X: {
+      fov += 0.1;
+	  break;
+	}
+	case Qt::Key_7: {
+		theta += 0.1;
+		break;
+	}
+	case Qt::Key_8: {
+		theta -= 0.1;
+		break;
+	}
+	case Qt::Key_4: {
+		phi += 0.1;
+		break;
+	}
+	case Qt::Key_5: {
+		phi -= 0.1;
+		break;
+	}
+	case Qt::Key_1: {
+		psi += 0.1;
+		break;
+	}
+	case Qt::Key_2: {
+		psi -= 0.1;
+		break;
+	}
+    default: event->ignore(); break;
+  }
+  update();
+}
+
+void MyGLWidget::mousePressEvent(QMouseEvent *e) {
+	//if (e->buttons() != Qt::LeftButton) return;
+	std::cout << "Pressing on " << e->x() << " " << e->y() << std::endl;
+	mousePos[0] = e->x();
+	mousePos[1] = e->y();
+}
+
+void MyGLWidget::mouseReleaseEvent(QMouseEvent *e) {
+	//if (e->buttons() != Qt::LeftButton) return;
+	std::cout << "Releasing on " << e->x() << " " << e->y() << std::endl;
+	if (e->x() - mousePos[0] > 25) {
+		psi -= 0.1*((e->x()-mousePos[0])/50);
+	}
+	else if (mousePos[0] - e->x() > 25) {
+		psi += 0.1*(-(e->x()-mousePos[0])/50);
+	}
+	if (mousePos[1] - e->y() > 25) {
+		theta += 0.1*(-(e->y()-mousePos[1])/50);
+	}
+	else if (e->y() - mousePos[1] > 25) {
+		theta -= 0.1*((e->y()-mousePos[1])/50);
+	}
+	update();
+}
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *e) {
+	//std::cout << "Moving on " << e->x() << " " << e->y() << std::endl;
+}
+
+
+void MyGLWidget::creaBuffers () 
+{
+  m.load("../../models/Patricio.obj");
+
+  glGenVertexArrays(1, &VAO_Model);
+  glBindVertexArray(VAO_Model);
+
+  GLuint VBO_Model_Pos;
+  glGenBuffers(1, &VBO_Model_Pos);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Model_Pos);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m.faces().size()*3*3, m.VBO_vertices(), GL_STATIC_DRAW);
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  GLuint VBO_Model_Col;
+  glGenBuffers(1, &VBO_Model_Col);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Model_Col);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m.faces().size()*3*3, m.VBO_matdiff(), GL_STATIC_DRAW);
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc); 
+
+  glm::vec3 posicio[6] = {
+	glm::vec3(2.5, 0, 2.5),
+	glm::vec3(2.5, 0, -2.5),
+	glm::vec3(-2.5, 0, -2.5),
+	
+	glm::vec3(2.5, 0, 2.5),
+	glm::vec3(-2.5, 0, -2.5),
+	glm::vec3(-2.5, 0, 2.5)
+  }; 
+   glm::vec3 color[6] = {
+	glm::vec3(1,0,0),
+	glm::vec3(0,0,1),
+	glm::vec3(0,1,0),
+	
+	glm::vec3(1,0,0),
+	glm::vec3(0,1,0),
+	glm::vec3(0,0,1),
+  };
+  
+  glGenVertexArrays(1, &VAO_Terra);
+  glBindVertexArray(VAO_Terra);
+  
+  GLuint VBO_Terra_Pos;
+  glGenBuffers(1, &VBO_Terra_Pos);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra_Pos);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(posicio), posicio, GL_STATIC_DRAW);
+  
+  // Activem l'atribut vertexLoc
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+  
+  GLuint VBO_Terra_Col;
+  glGenBuffers(1, &VBO_Terra_Col);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra_Col);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+  // Activem l'atribut colorLoc
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc);
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/basicShader.frag");
+  vs.compileSourceFile("shaders/basicShader.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “color” del vertex shader
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+  // Uniform locations
+  transLoc = glGetUniformLocation(program->programId(), "TG");
+  projLoc = glGetUniformLocation(program->programId(), "proj");
+  viewLoc = glGetUniformLocation(program->programId(), "view");
+}
+
---
+++ Sessio6/ex4/MyGLWidget.h
@@ -0,0 +1,68 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "model.h"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core 
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    virtual void keyPressEvent (QKeyEvent *event);
+	virtual void mousePressEvent(QMouseEvent *e);
+	virtual void mouseReleaseEvent(QMouseEvent *e);
+	virtual void mouseMoveEvent(QMouseEvent *e); 
+  
+  private:
+    void creaBuffers();
+    void carregaShaders();
+    void modelTransform(bool terra, int num_patricio);
+    void projectTransform();
+    void viewTransform();
+    void iniCamera();
+    void calcCenterRadius();
+	void calcModelBox();
+
+    // attribute locations
+    GLuint vertexLoc, colorLoc;
+    // uniform locations
+    GLuint transLoc;
+    GLuint projLoc;
+    GLuint viewLoc;
+    // VAO i VBO names
+    GLuint VAO_Model, VAO_Terra;
+    // Program
+    QOpenGLShaderProgram *program;
+    // Viewport
+    GLint ample, alt;
+    // Internal vars
+    Model m;
+    GLint mousePos[2];
+    float scale, scale2, angle;
+    float fov, fov_ini, ra, znear, zfar;
+    glm::vec3 OBS, VRP, VUP;
+    glm::vec3 pos;
+    glm::vec3 center, patricio_center_base;
+    float radius, dist;
+    float psi, theta, phi;
+    glm::vec3 min_scene, max_scene;
+    bool perspective;
+};
+
